b473cead1ec067a57bb41de3f6351b621d776b49
Yen Dah Hsiang <dustin0116@gmail.com>
2024-11-24 20:22:47 -0800

    Updated API

--- routers/crimes.py
+++ routers/crimes.py
@@ -7,11 +7,16 @@ This is the module that sets the APIs.
 Dependencies:
 -------------
 - APIRouter: Set the API URL route.
+- HTTPException: For raising HTTP errors.
+- SQLAlchemy: For extracting query results and error checking.
+- Session: Used for opening a session for database writing.
+- Crime: The ORM Model Class for crimes.
 '''
 import logging
 
-from fastapi import APIRouter
-from sqlalchemy import extract
+from fastapi import APIRouter, HTTPException
+from sqlalchemy import extract, select
+from sqlalchemy.exc import SQLAlchemyError
 
 from ..database.pool import Session
 from ..models.crime_orm import Crime
@@ -21,21 +26,33 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)  # Logs SQL statem
 
 router = APIRouter(prefix='/crimes')
 
-@router.get('/total')
-async def read_total_crimes(year: int):
-    ''' Returns the total number of crimes. '''
-    # session = Session()
-    # # Query unique years
-    # years = session.query(extract('year', Crime.event_datetime)).distinct().all()
-    # years = [year[0] for year in years]
-    return { 'total': year }
-
 @router.get('/all_years')
 async def read_all_years():
     ''' Returns the years of the dataset. '''
     session = Session()
     # Query unique years
-    years = session.query(extract('year', Crime.event_datetime)).distinct().all()
-    logging.info(years)
-    years = [year[0] for year in years]
-    return { 'years': years }
+    try:
+        years = session.query(extract('year', Crime.event_datetime)).distinct().all()
+        if not years:
+            raise HTTPException(status_code=404, detail="Years not found in database")
+        years = [year[0] for year in years]
+        return { 'years': years }
+    except SQLAlchemyError as e:
+        print(f'An error occurred: {e}')
+        raise HTTPException(status_code=500, detail="Database error occurred") from e
+    finally:
+        session.close()
+
+@router.get('/all')
+async def read_all_crimes(year: int):
+    ''' Returns all of the crimes within the specified year in the database. '''
+    session = Session()
+    # Query unique years
+    try:
+        all_crimes = session.execute(select(Crime).where(Crime.event_datetime == year))
+        return all_crimes
+    except SQLAlchemyError as e:
+        print(f'An error occurred: {e}')
+        raise HTTPException(status_code=500, detail="Database error occurred") from e
+    finally:
+        session.close()
