d327d8d1467b6a8c1e3a13dc0f0fd856244de507
Yen Dah Hsiang <dustin0116@gmail.com>
2024-11-24 23:05:12 -0800

    Added crimes of specific year API

--- routers/crimes.py
+++ routers/crimes.py
@@ -12,11 +12,14 @@ Dependencies:
 - Session: Used for opening a session for database writing.
 - Crime: The ORM Model Class for crimes.
 '''
+import json
 import logging
+from datetime import datetime
+from decimal import Decimal
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from sqlalchemy import extract, select
-from sqlalchemy.exc import SQLAlchemyError
 
 from ..database.pool import Session
 from ..models.crime_orm import Crime
@@ -25,34 +28,46 @@ logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)  # Logs SQL statements
 
 router = APIRouter()
+session = Session()
+
+def crime_json(crime):
+    ''' Get JSON of a crime '''
+    print(crime.id)
+    return {
+        'id': crime.id,
+        'case': crime.case,
+        'date': crime.event_datetime.isoformat(),
+        'address': crime.hundred_block,
+        'neighborhood': crime.neighborhood,
+        'x': crime.x,
+        'y': crime.y
+    }
+
 
 @router.get('/years')
 async def read_all_years():
     ''' Returns the years of the dataset. '''
-    session = Session()
-    # Query unique years
-    try:
-        years = session.query(extract('year', Crime.event_datetime)).distinct().all()
-        if not years:
-            raise HTTPException(status_code=404, detail="Years not found in database")
-        years = [year[0] for year in years]
-        return { 'years': years }
-    except SQLAlchemyError as e:
-        print(f'An error occurred: {e}')
-        raise HTTPException(status_code=500, detail="Database error occurred") from e
-    finally:
-        session.close()
+    with session.begin():
+    # Get distinct years
+        result = session.execute(select(extract('year', Crime.event_datetime)).distinct())
+        distinct_years = [row[0] for row in result]
+        distinct_years = ([ int(year)
+                            if isinstance(year, Decimal)
+                            else year for year in distinct_years
+                         ])
 
-@router.get('/crimes')
+        return JSONResponse(content={'years': distinct_years})
+
+@router.get('/crimes/{year}')
 async def read_all_crimes(year: int):
     ''' Returns all of the crimes within the specified year in the database. '''
-    session = Session()
-    # Query unique years
-    try:
-        all_crimes = session.execute(select(Crime).where(Crime.event_datetime == year))
-        return all_crimes
-    except SQLAlchemyError as e:
-        print(f'An error occurred: {e}')
-        raise HTTPException(status_code=500, detail="Database error occurred") from e
-    finally:
-        session.close()
+    with session.begin():
+        start_date = datetime(year, 1, 1)
+        end_date = datetime(year+1, 1, 1)
+        query = ( select(Crime)
+                 .where(Crime.event_datetime >= start_date)
+                 .where(Crime.event_datetime < end_date)
+                )
+        result = session.execute(query).all()
+        crimes = [crime_json(crime[0]) for crime in result]  # Convert each row to dict
+        return JSONResponse(content=crimes)
