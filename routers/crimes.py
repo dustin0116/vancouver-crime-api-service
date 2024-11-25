6e55fbc25e9842daf466298ebde3118d3fc93229
Yen Dah Hsiang <dustin0116@gmail.com>
2024-11-24 16:06:15 -0800

    Added API for getting all available years

--- routers/crimes.py
+++ routers/crimes.py
@@ -8,11 +8,34 @@ Dependencies:
 -------------
 - APIRouter: Set the API URL route.
 '''
+import logging
+
 from fastapi import APIRouter
+from sqlalchemy import extract
+
+from ..database.pool import Session
+from ..models.crime_orm import Crime
+
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)  # Logs SQL statements
 
 router = APIRouter(prefix='/crimes')
 
 @router.get('/total')
 async def read_total_crimes(year: int):
     ''' Returns the total number of crimes. '''
-    return { 'message': year }
+    # session = Session()
+    # # Query unique years
+    # years = session.query(extract('year', Crime.event_datetime)).distinct().all()
+    # years = [year[0] for year in years]
+    return { 'total': year }
+
+@router.get('/all_years')
+async def read_all_years():
+    ''' Returns the years of the dataset. '''
+    session = Session()
+    # Query unique years
+    years = session.query(extract('year', Crime.event_datetime)).distinct().all()
+    logging.info(years)
+    years = [year[0] for year in years]
+    return { 'years': years }
